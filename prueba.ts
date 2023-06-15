2f1be0e3c71c033efd17732ec78fe1cfece47380
Raul <raul.alhena@gmail.com>
2023-06-15 14:10:45 +0200

    nothing changed

--- prueba.ts
+++ prueba.ts
@@ -1,7 +1,13 @@
-const o = { name: "This is 'you'" };
+const o = {
+	"name": "This is 'you'",
+	"una y otra": "Other"
+};
 
 const a = (o) => {
 	console.log(o.name);
+	if (o === 1) {
+		console.log("a");
+	}
 };
 
 a(o);
