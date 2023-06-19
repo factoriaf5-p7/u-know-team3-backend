447f00006c5470526f83f60ee0d467b51c4fdd74
Raul <raul.alhena@gmail.com>
2023-06-19 20:26:30 +0200

    commented Post from user.controller

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -8,10 +8,10 @@ import { UpdateUserDto } from './dto/update-user.dto';
 export class UsersController {
 	constructor(private readonly usersService: UsersService) {}
 
-  @Post()
-	create(@Body() createUserDto: CreateUserDto) {
-		return this.usersService.create(createUserDto);
-	}
+  // @Post()
+	// create(@Body() createUserDto: CreateUserDto) {
+	// 	return this.usersService.create(createUserDto);
+	// }
 
   @Get()
   findAll() {
