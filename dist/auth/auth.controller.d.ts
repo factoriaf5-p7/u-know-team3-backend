a37dd01f9eb7b35ad7e7a047f3458904a188fa69
andreubltzr <andreubltzr11@gmail.com>
2023-06-19 17:44:27 +0200

    deleted dist

--- dist/auth/auth.controller.d.ts
+++
@@ -1,7 +0,0 @@
-import { AuthService } from './auth.service';
-import { GetUserLoginDto } from './dto/get-user-login.dto';
-export declare class AuthController {
-    private readonly authService;
-    constructor(authService: AuthService);
-    findOne(user: GetUserLoginDto): Promise<any>;
-}
--- dist/auth/auth.service.d.ts
+++
@@ -1,7 +0,0 @@
-import { UsersService } from 'src/users/users.service';
-import { GetUserLoginDto } from './dto/get-user-login.dto';
-export declare class AuthService {
-    private userService;
-    constructor(userService: UsersService);
-    findOne(user: GetUserLoginDto): Promise<any>;
-}
