94602019f1d8b437ef06593c3e80ba810612d0cc
Angela-GM <angelagm0112@gmail.com>
2023-06-23 10:39:46 +0200

    new dump courses and users

--- src/courses/schemas/course.schema.ts
+++ src/courses/schemas/course.schema.ts
@@ -27,8 +27,11 @@ export class Course {
   @Prop({ default: false })
   	bought: boolean;
 
-  @Prop({ default: Date.now() })
+    @Prop({ default: Date.now() })
   	create_date: Date;
+
+    @Prop({ default: Date.now() })
+  	update_date: Date;
 
           @ApiProperty( { example: '# Introduction to Web Development\n\nWelcome to the Introduction to Web Development course! In this course, you will learn the fundamentals of web development using HTML, CSS, and JavaScript.\n\n## Course Outline\n\n### Chapter 1: HTML Basics\n\nIn this chapter, we will cover the following topics:\n\n- Introduction to HTML\n- HTML tags and elements\n- Building a basic HTML webpage\n\n### Chapter 2: CSS Styling\n\nIn this chapter, we will cover the following topics:\n\n- Introduction to CSS\n- Selectors and properties\n- Applying styles to HTML elements\n\n### Chapter 3: JavaScript Fundamentals\n\nIn this chapter, we will cover the following topics:\n\n- Introduction to JavaScript\n- Variables and data types\n- Control flow and functions\n\n## Conclusion\n\nCongratulations on completing the Introduction to Web Development course! We hope you\'ve gained a solid foundation in web development.' } )
 
